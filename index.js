13bf823a46f2650ab5d35bbd0cdf3d1079343f49
idriss <idrissaamadouousmane@gmail.com>
2023-09-20 15:48:26 +0100

    Projet final

--- index.js
+++ index.js
@@ -17,10 +17,31 @@ end_date.min = tomorrowFormat;
 start_date.addEventListener("change", (e) => {
     let day = new Date(e.target.value);
 
-    if (end_date.value < start_date.value) {
+    if (end_date.value <= start_date.value) {
         day.setDate(day.getDate() + 1);
         end_date.value = day.toISOString().split("T")[0];
-        console.log(day);
     }
-})
+});
 
+end_date.addEventListener("change", (e) => {
+    let day = new Date(e.target.value);
+
+    if (end_date.value <= start_date.value) {
+        day.setDate(day.getDate() -1);
+        start_date.value = day.toISOString().split("T")[0];    
+    }
+});
+
+// Calcul du montant
+const bookingCalc = () => {
+    let diffTime = Math.abs(
+        new Date(end_date.value) - new Date(start_date.value)
+    );
+    let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    total.textContent = diffDays * nightPrice.textContent;
+}
+start_date.addEventListener("change", bookingCalc);
+end_date.addEventListener("change", bookingCalc);
+
+bookingCalc();
